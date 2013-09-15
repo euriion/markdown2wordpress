8ae6c3b65e168e1c6a9083b6a8d6fbc7ae8ac33a
Christian Mayer <thefox21at@gmail.com>
2013-09-15 17:59:49 +0200

    sleep

--- posts.sh
+++ posts.sh
@@ -4,8 +4,8 @@ for file in posts/*; do
 	
 	title=$(grep -i Title: $file | sed 's/Title: //')
 	
-	echo "file: '$file', title '$title'"
-	php import.php -t "$title" -f $file -s
-	break
+	#echo "file: '$file', title '$title'"
+	php import.php -t "$title" -f $file -s && mv $file posted
+	sleep 5
 	
 done
